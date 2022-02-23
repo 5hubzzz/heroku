f315042168031a2d0058509e72d501dd43a2a055
5hubzzz <89408379+5hubzzz@users.noreply.github.com>
2022-02-22 22:21:12 -0800

    Update index.php

--- index.php
+++ index.php
@@ -4,23 +4,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DBMS Practical exam </title>
+    <title>Boat Club</title>
 </head>
 <body>
- <h2>welcome to hotel management system</h2>
- <p>Enter your details</p>
+    <h2>BOAT CLUB!</h2>
+    <p>welcome to the boat club!</p>
 
- <form action="index.php" method="post">
-     <input type="text" name="name" id="name" placeholder="Enter your name">
-     <input type="text" name="age" id="age" placeholder="Enter your age">
-     <input type="text" name="gender" id="gender" placeholder="Enter your gender">
-     <input type="phone" name="phone" id="phone" placeholder="Enter your phone number">
-     <input type="email" name="email" id="email" placeholder="Enter your email id">
-     <button class="btn" >Submit</button>
- </form>
- <form action = "search.php" method = "post">
- <input type="text" name="id" id="id" placeholder="Enter the id to search">
- <button class= "btn" > search </button>
-</form>
+    <form action="conn.php" method="post">
+        <input type="text" name="name" id="name" placeholder="Enter your name!">
+        <input type="text" name="address" id="address" placeholder="Enter your adress!">
+        <input type="phone" name="phone" id="phone" placeholder="enter your phone number!">
+        <button type="submit">Submit</button>
+    </form>
+    <form action="search.php" method="post">
+        <input type="text" name="id" id="id" placeholder="enter the sailor id!">
+        <button type = "search">search</button>
+    </form>
 </body>
 </html>
